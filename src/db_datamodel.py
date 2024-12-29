aa9ba9e7b949c61cecfe60e9d11985560be9f576
Michael Rath <michael_rath@web.de>
2024-12-29 11:57:26 +0100

    GEP-011: update schema

--- src/db_datamodel.py
+++ src/db_datamodel.py
@@ -4,14 +4,13 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
-DB_NAME = pathlib.Path(__file__) / '..' / '..' / 'db' / 'geschichte_eur0pas.db' # TODO(micha): strange path with double ..
-
+DB_NAME = pathlib.Path(__file__).parent / '..' / 'db' / 'geschichte_eur0pas.db'
 
 Base = declarative_base()
 
-
 Episode_2_episode = Table(
-    'episode_2_episode', Base.metadata,
+    'episode_2_episode',
+    Base.metadata,
     Column('from_id', Integer,
            ForeignKey('episode.id'),
            primary_key=True),
@@ -21,41 +20,56 @@ Episode_2_episode = Table(
 )
 
 
+Episode_2_keyword = Table(
+    'episode_2_keyword',
+    Base.metadata,
+    Column('episode_id', Integer,
+           ForeignKey('episode.id'),
+           primary_key=True),
+    Column('keyword_id', Integer,
+            ForeignKey('keyword.id'),
+            primary_key=True)
+)
+
+
 class Episode(Base):
-  __tablename__ = "episode"
+    __tablename__ = "episode"
 
-  id: Mapped[int] = mapped_column(primary_key=True)
-  title = Column(String)
-  link = Column(String)
-  number = Column(Integer)
-  publication_date = Column(String)
-  duration_seconds = Column(Integer)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title = Column(String)
+    link = Column(String)
+    number = Column(Integer)
+    publication_date = Column(String)
+    duration_seconds = Column(Integer)
 
-  category_id: Mapped[int] = mapped_column(ForeignKey('category.id'))
-  category: Mapped['Category'] = relationship(back_populates='episodes')
+    category_id: Mapped[int] = mapped_column(ForeignKey('category.id'))
+    category: Mapped['Category'] = relationship(back_populates='episodes')
 
-  linked_episodes = relationship(
-    'Episode',
-    secondary=Episode_2_episode,
-    primaryjoin=id==Episode_2_episode.c.from_id,
-    secondaryjoin=id==Episode_2_episode.c.to_id)
+    linked_episodes = relationship(
+        'Episode',
+        secondary=Episode_2_episode,
+        primaryjoin=id==Episode_2_episode.c.from_id,
+        secondaryjoin=id==Episode_2_episode.c.to_id)
+
+    keywords = relationship('Keyword', secondary=Episode_2_keyword, back_populates='episodes')
 
 
 class Category(Base):
-  __tablename__ = 'category'
+    __tablename__ = 'category'
 
-  id: Mapped[int] = mapped_column(primary_key=True)
-  marker = Column(String)
-  currated_name = Column(String)
-  organic_names = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    marker = Column(String)
+    curated_name = Column(String)
+    # separated by '|'
+    organic_names = Column(String)
 
-  episodes: Mapped[List['Episode']] = relationship(back_populates='category')
+    episodes: Mapped[List['Episode']] = relationship(back_populates='category')
 
 
 class Keyword(Base):
-  __tablename__ = 'keyword'
+    __tablename__ = 'keyword'
 
-  id: Mapped[int] = mapped_column(primary_key=True)
-  name = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name = Column(String)
 
-  episode_id: Mapped[int] = mapped_column(ForeignKey('episode.id'), nullable=True)
+    episodes = relationship('Episode', secondary=Episode_2_keyword, back_populates='keywords')
